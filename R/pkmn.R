ffceab4a72a87eefc1078abd62fa904533267e18
Ryan Schneider <ryanaschneider11@gmail.com>
2022-04-01 13:01:53 -0400

    stuff
    ???

--- R/pkmn.R
+++ R/pkmn.R
@@ -7,7 +7,6 @@
 #' \describe{
 #'    \item {name}{chr denoting the name of each Pokemon}
 #'    \item {type_1}{fct The pokemon's first/primary type.}
-#'    \item {type_level_code}{fct A numerical coding version of the first/primary type column, for data analysis}
 #'    \item {total}{dbl The pokemon's total aggregate base stats}
 #'    \item {hp}{dbl The base HP (Hit Points) of each pokemon}
 #'    \item {attack}{dbl Base Attack points}
@@ -16,7 +15,7 @@
 #'    \item {sp_def}{dbl Base Special Defense points}
 #'    \item {speed}{dbl Base Speed points}
 #'    \item {generation}{fct Numerical factor, starting with 1, that corresponds to each Pokemon's generation}
-#'    \item {legendary}{fct A factor that has levels TRUE or FALSE, indicating the stats of a pokemon as Legendary or not}
+#'    \item {legendary}{fct A factor that has levels Yes or No, indicating the stats of a pokemon as Legendary or not}
 #'    }
 #'    @source \url{https://www.kaggle.com/abcsds/pokemon}
 #'    "pkmn"
